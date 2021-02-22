27d337cc67095ecc65c10e74870af2f4c58a83a0
Tresor <tresor.moise2001@gmail.com>
2021-02-22 10:55:51 +0200

    clean up the folders

---
+++ src/containers/Generate.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+const arr=["damn","woo","hello","more","riseUp","Godamn!"]
+const Generate=()=>{
+    const [quotes,setQuotes]=useState(0)
+    const handleS=()=>{
+        setQuotes(Math.round(Math.random() * 4))
+    }
+    return(
+        <button type="button" onClick={handleS} className="bg-green-400 rounded-full p-1 shadow">
+            {arr[quotes]}
+        </button>
+    )
+}
+
+export default Generate;
