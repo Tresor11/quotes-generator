87de6b66272ef26250da8904ca75b68629be5d8b
Tresor <tresor.moise2001@gmail.com>
2021-02-26 22:14:52 +0200

    add the prop types for the Quotes container

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,8 @@
 import Generate from '../containers/Generate'
+import PropTypes from 'prop-types';
 import '../styles/Quote.css';
 
-const Quote=()=>{
+const Quote=(props)=>{
     return(
         <div className="quote-cont p-2 my-52 mx-52 bg-white shadow rounded">
             <p>This is a quote</p>
@@ -11,5 +12,9 @@ const Quote=()=>{
         </div>
     )
 }
+
+Quote.propTypes = {
+    quotes: PropTypes.string
+};
 
 export default Quote;
--- src/containers/Generate.js
+++ src/containers/Generate.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
-const arr=["damn","woo","hello","more","riseUp","Godamn!"]
-const Generate=()=>{
-    const [quotes,setQuotes]=useState(0)
-    const handleS=()=>{
-        setQuotes(Math.round(Math.random() * 4))
-    }
-    return(
-        <button type="button" onClick={handleS} className="bg-green-400 rounded-full p-1 shadow">
-            New Quote
-        </button>
-    )
-}
+import React, { useState } from "react";
+const arr = ["damn", "woo", "hello", "more", "riseUp", "Godamn!"];
+const Generate = () => {
+  const [quotes, setQuotes] = useState(0);
+  const handleS = () => {
+    setQuotes(Math.round(Math.random() * 4));
+  };
+  return (
+    <button
+      type="button"
+      onClick={handleS}
+      className="bg-green-400 rounded-full p-1 shadow"
+    >
+      New Quote
+    </button>
+  );
+};
 
 export default Generate;
