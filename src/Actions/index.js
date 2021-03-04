182600b3e81c99713f0daafecb43ef0a6c1d0dbf
Tresor <tresor.moise2001@gmail.com>
2021-03-04 19:32:02 +0200

    update the actions folder

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,5 +1,24 @@
-const FETCHING={
-    type:"FETCHING",
-}
+const fetchQuotesPending = () => ({
+  type: "FETCH_QUOTES_PENDING",
+});
 
-export default FETCHING;
+const fetchQuotesSuccess = (quotes) => ({
+  type: "FETCH_QUOTES_SUCCESS",
+  quotes,
+});
+
+const fetchQuotesFailed = () => ({
+  type: "FETCH_QUOTES_FAILED",
+});
+
+const changeCategory = (category) => ({
+  type: "CHANGE_CATEGORY",
+  category,
+});
+
+const addFavorite = (id) => ({
+  type: "ADD_FAVORITE",
+  id,
+});
+
+export default { fetchQuotesSuccess, fetchQuotesFailed, fetchQuotesPending };
