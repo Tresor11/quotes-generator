ba45dcf1c59e9b2c78600251d5b0f74dffaf1480
Tresor <tresor.moise2001@gmail.com>
2021-03-12 09:29:25 +0200

    add buttons

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,15 +3,24 @@ import PropTypes from 'prop-types';
 import '../styles/Quote.css';
 
 const Quote=(props)=>{
+    const {quote}=props
     return(
         <div className="quote-cont p-2 my-52 mx-52 bg-white shadow rounded">
-            <Generate/>
+            <div className="text-cont text-center">
+                {quote}
+            </div>
+            <div className="button cont">
+                <span class="copy-text"></span>
+                <span class="add-favorite"></span>
+                <span class="share-twitter"></span>
+                <span class="share-facebook"></span>
+            </div>
         </div>
     )
 }
 
 Quote.propTypes = {
-    quotes: PropTypes.string
+    quote: PropTypes.string
 };
 
 export default Quote;
